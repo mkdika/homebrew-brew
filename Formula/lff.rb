b73072316c678b4fd4f743ae3d13a0c31dc53943
Maikel Chandika <mkdika@gmail.com>
2020-05-07 08:13:41 +0700

    Update lff Formula to v0.1.1

--- Formula/lff.rb
+++ Formula/lff.rb
@@ -1,8 +1,8 @@
 class Lff < Formula
   desc "Simple and straightforward large files finder utility for *nix, optimize for human."
   homepage "https://mkdika.github.io/"
-  url "https://github.com/mkdika/lff-cr/archive/v0.1.0.tar.gz"
-  sha256 "676ad6ac7783b1df8c27cb118debadfe16a972ed3ef6d7d9583e1eaced48f149"
+  url "https://github.com/mkdika/lff-cr/archive/v0.1.1.tar.gz"
+  sha256 "99d0b038cc09062dea3de0809bea05fa7816a948489d728b0e6816548ba64198"
 
   depends_on "crystal" => :build
 
@@ -14,6 +14,6 @@ class Lff < Formula
   end
 
   test do
-    assert_equal "lff version 0.1.0", pipe_output("#{bin}/lff -v")
+    assert_equal "lff version 0.1.1", pipe_output("#{bin}/lff -v")
   end
 end
